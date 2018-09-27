6b91421eb9a9693450a262273bec820477f0f0e8
Slađan Ristić <sladiri@users.noreply.github.com>
2018-09-27 17:08:45 +0200

    add vega and vega-lite examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { WorkerService } from "./services/worker/worker.service";
+import { View, Spec } from "vega";
+
+declare var vega: any;
+declare var vegaEmbed: (containerId: string, spec: any) => void;
 
 @Component({
   selector: "app-root",
@@ -8,6 +12,9 @@ import { WorkerService } from "./services/worker/worker.service";
 })
 export class AppComponent implements OnInit {
   title = "web-worker-v7";
+
+  view: View;
+  viewLite: View;
 
   // TS does not parse async generators as class method correctly
   foo = async function*() {
@@ -22,9 +29,54 @@ export class AppComponent implements OnInit {
   constructor(private workerService: WorkerService) {}
 
   async ngOnInit() {
+    await this.vegaInit();
+    await this.vegaLiteInit();
+
     const reference = Object.create(null);
     await this.testWorker(reference, "main");
     await this.testWorker(reference, "another");
+  }
+
+  private async vegaInit() {
+    const spec: Spec = JSON.parse(
+      await vega
+        .loader()
+        .load("https://vega.github.io/vega/examples/bar-chart.vg.json"),
+    );
+    this.view = new vega.View(vega.parse(spec))
+      // .renderer("svg")
+      .renderer("canvas")
+      .initialize("#vegaTest") // initialize view within parent DOM container
+      // .width(300)
+      // .height(300)
+      .hover() // enable hover encode set processing
+      .run();
+  }
+
+  private async vegaLiteInit() {
+    const spec = {
+      $schema: "https://vega.github.io/schema/vega-lite/v3.0.0-rc6.json",
+      description: "A simple bar chart with embedded data.",
+      data: {
+        values: [
+          { a: "A", b: 28 },
+          { a: "B", b: 55 },
+          { a: "C", b: 43 },
+          { a: "D", b: 91 },
+          { a: "E", b: 81 },
+          { a: "F", b: 53 },
+          { a: "G", b: 19 },
+          { a: "H", b: 87 },
+          { a: "I", b: 52 },
+        ],
+      },
+      mark: "bar",
+      encoding: {
+        x: { field: "a", type: "ordinal" },
+        y: { field: "b", type: "quantitative" },
+      },
+    };
+    vegaEmbed("#vegaLiteTest", spec);
   }
 
   private async testWorker(reference: any, workerName: string) {
