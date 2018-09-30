11579701563b5a86ccb5d5d91739070327e83251
Slađan Ristić <sladiri@users.noreply.github.com>
2018-09-30 19:15:28 +0200

    remove parameter from service, breaks AOT
    https://github.com/angular/angular/issues/24533

--- src/app/services/window/window.service.ts
+++ src/app/services/window/window.service.ts
@@ -1,19 +1,12 @@
-import { Injectable, Optional, PLATFORM_ID, Inject } from "@angular/core";
+import { Injectable, PLATFORM_ID, Inject } from "@angular/core";
 import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({
   providedIn: "root",
 })
 export class WindowService {
-  constructor(
-    @Inject(PLATFORM_ID) platformId: Object,
-    @Optional()
-    @Inject(Window)
-    _window: Window,
-  ) {
-    if (_window !== null) {
-      this.window = _window;
-    } else if (isPlatformBrowser(platformId)) {
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    if (isPlatformBrowser(platformId)) {
       this.window = window;
     } else {
       const navigator = Object.assign(Object.create(null), { appVersion: "" });
