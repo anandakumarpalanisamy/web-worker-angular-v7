e4b3cb01c1898e382f91f9807711b5423652a814
Slađan Ristić <sladiri@users.noreply.github.com>
2018-09-30 18:49:56 +0200

    fix bug in iterable-queue

--- src/iterable-queue.ts
+++ src/iterable-queue.ts
@@ -7,48 +7,54 @@ export const IterableQueue = (maxCapacity = 2 ** 12, minCapacity = 2 ** 4) => {
   let values = createArray(minCapacity);
   let readPtr = 0;
   let writePtr = 0;
-  let itemCount = 0;
 
   const done = () => readPtr === writePtr;
+  const length = () => writePtr - readPtr;
+  const reset = () => {
+    writePtr = readPtr = 0;
+  };
+  const reduceCapacity = () => {
+    if (length() === 0) {
+      reset();
+    }
+    if (values.length / 2 >= minCapacity && length() < values.length / 2) {
+      values = values
+        .slice(readPtr, readPtr + length())
+        .concat(createArray(values.length / 2 - length()));
+      writePtr = length();
+      readPtr = 0;
+      console.assert(readPtr >= 0);
+      console.assert(writePtr >= 0);
+    }
+  };
   const remove = () => {
     if (done()) {
-      writePtr = readPtr = 0;
+      reset();
       return;
     }
     const value = values[readPtr];
     values[readPtr] = undefined;
     readPtr += 1;
-    if (
-      writePtr - readPtr >= minCapacity &&
-      writePtr - readPtr < values.length / 2
-    ) {
-      values = values.slice(readPtr, values.length / 2 + readPtr);
-      writePtr = writePtr - readPtr;
-      readPtr = 0;
-      console.assert(readPtr >= 0);
-      console.assert(writePtr >= 0);
-    }
-    itemCount -= 1;
+    reduceCapacity();
     return value;
   };
   const add = value => {
-    if (writePtr + 1 > values.length) {
+    if (length() + 1 > values.length) {
       if (values.length * 2 > maxCapacity) {
-        console.error("queue reached max capacity");
+        console.error("queue reached max capacity", values.length, maxCapacity);
         return;
       }
       values.push(...createArray(values.length));
     }
     values[writePtr] = value;
     writePtr += 1;
-    itemCount += 1;
   };
 
   return Object.assign(Object.create(null), {
     // Queue
     remove,
     add,
-    length: () => itemCount,
+    length,
     // Iterator
     [Symbol.iterator]() {
       return this;
